e30254a90194f8cef38ce82682796164a8b76ad1
Janssen <gundranjanjan@gmail.com>
2026-02-16 12:09:36 +0800

    For Deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home-component/home-component.component'; 
-import { CertificateDownloadComponent } from './certficate-download/certficate-download.component';
 import { RegisterComponent } from './register/register.component';
 import { BirthComponent } from './certificate_types/birth/birth.component';
 import { AccountComponent } from './account/account.component';
-import { PendingComponent } from './pending/pending.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { AdminUsersComponent } from './admin-users/admin-users.component';
 import { OtpVerificationComponent } from './otpverify/otpverify.component';
@@ -38,15 +36,12 @@ export const routes: Routes = [
       path: 'login',
       loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
     },
-    { path: 'approved', component: CertificateDownloadComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'otpverify', component: OtpVerificationComponent },
-    { path: 'downloads', component: CertificateDownloadComponent },
     { path: 'birth', component: BirthComponent },
     { path: 'account-settings', component: AccountComponent },
     { path: 'request-detail/:id', component: RequestDetailComponent },
     { path: 'account', component: AccountComponent },
-    { path: 'pending', component: PendingComponent },
     { path: 'reset-password', component: ResetPasswordComponent },
     { path: 'admin-users', component: AdminUsersComponent, canActivate: [authGuard] },
     { path: 'edit-profile', component: EditProfileComponent, canActivate: [authGuard] },
